a6510067c7aa8c65e03051350ac8d4af379065f3
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-12-01 23:46:50 -0300

    📝 update(mobile): group tests with Describe

--- src/utils/getNextDays.test.ts
+++ src/utils/getNextDays.test.ts
@@ -1,8 +1,10 @@
 import { getNextDays } from "./getNextDays";
 
-test('should be return the next five days', () => {
-  const days = getNextDays();
-  console.log(days);
+describe('getNextDays', () => {
+  it('should be return the next five days', () => {
+    const days = getNextDays();
+    console.log(days);
 
-  expect(days.length).toBe(5);
+    expect(days.length).toBe(5);
+  });
 });
