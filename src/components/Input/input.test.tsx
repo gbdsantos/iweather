e048bb444dbf05daffc7d7f88bd2807d04ea1125
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-12-08 15:05:11 -0300

    🧪 test(mobile): understand get and query

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -4,9 +4,9 @@ import { Input } from "@components/Input";
 
 describe("Component: Input", () => {
   it("should be render without activity indicator if isLoading prop is undefined.", () => {
-    render(<Input isLoading />);
+    render(<Input />);
 
-    const activityIndicator = screen.getByTestId("activity-indicator");
-    console.log(activityIndicator);
+    const activityIndicator = screen.queryByTestId("activity-indicator");
+    expect(activityIndicator).toBeNull();
   });
 });
