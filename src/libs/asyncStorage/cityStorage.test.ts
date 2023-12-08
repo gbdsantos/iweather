6fc39750b52b934d788f78d980a9c93846973911
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-12-08 17:35:00 -0300

    ⚙️ config(mobile): Async Storage mock

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -3,6 +3,6 @@ import { getStorageCity } from "@libs/asyncStorage/cityStorage";
 describe("Storage: CityStorage", () => {
   it("should be return null when don't have a city storaged.", async () => {
     const response = await getStorageCity();
-    console.log(response);
+    expect(response).toBeNull();
   });
 });
