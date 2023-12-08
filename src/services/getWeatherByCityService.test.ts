f2ff258bbbc86407208414e1137b3cf0ea787621
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-12-08 17:13:50 -0300

    🧪 test(mobile): testing API return

---
+++ src/services/getWeatherByCityService.test.ts
@@ -0,0 +1,12 @@
+import { api } from "./api";
+import { mockWeatherAPIResponse } from "@__tests__/mocks/mockWeatherAPIResponse";
+import { getWeatherByCityService } from "./getWeatherByCityService";
+
+describe("Service: getWeatherByCityService", () => {
+  it("should be return weather api data formatted", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
+
+    const response = await getWeatherByCityService({ latitude: 123, longitude: 456});
+    expect(response).toHaveProperty("today");
+  });
+});
