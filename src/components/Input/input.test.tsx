68b2f42ebe4bc5078fe0ef8daebb9fa2247b4589
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-12-08 14:49:56 -0300

    🧪 test(mobile): render the component in the test

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -3,5 +3,7 @@ import { render } from "@testing-library/react-native";
 import { Input } from "@components/Input";
 
 describe("Component: Input", () => {
-
+  it("should be render without activity indicator", () => {
+    render(<Input />);
+  });
 });
