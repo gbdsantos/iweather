1cfd1a72312257d1a9a84431b80d6aadcd9c2dba
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-12-08 17:22:49 -0300

    🧪 test(mobile): native module mock

---
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -0,0 +1,8 @@
+import { getStorageCity } from "@libs/asyncStorage/cityStorage";
+
+describe("Storage: CityStorage", () => {
+  it("should be return null when don't have a city storaged.", async () => {
+    const response = await getStorageCity();
+    console.log(response);
+  });
+});
