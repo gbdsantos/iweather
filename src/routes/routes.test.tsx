82f87ecf1e8b07fa7edfce52a66f1fe2f558c6fe
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-12-14 21:14:46 -0300

    🧪 test(mobile): finish routes test

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,6 +1,8 @@
-import { render, screen, waitFor } from "@__tests__/utils/customRender";
+import { act, render, screen, waitFor } from "@__tests__/utils/customRender";
 import { Routes } from ".";
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
+import { api } from "@services/api";
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
 
 describe("Routes", () => {
   it("should be render search screen when not city selected", async () => {
@@ -11,6 +13,8 @@ describe("Routes", () => {
   });
 
   it("should be render Dashboard screen when has city selected.", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
+
     const city = {
       id: '1',
       name: 'São Paulo',
@@ -20,7 +24,9 @@ describe("Routes", () => {
 
     await saveStorageCity(city);
 
-    const { debug } = render(<Routes />);
-    debug();
+    await act(() => waitFor(() => render(<Routes />)));
+
+    const title = screen.getByText(city.name);
+    expect(title).toBeTruthy();
   });
 });
